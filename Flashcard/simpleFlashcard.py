09ffed033e679b1a004458abce9200fcd39480c4
albert c <albert.cheng@viasat.com>
2016-02-18 15:51:37 -0800

    added score keeping feature

--- Flashcard/simpleFlashcard.py
+++ Flashcard/simpleFlashcard.py
@@ -8,20 +8,30 @@ with open('vocab.txt', 'r') as vocab_list:
     vocab = [l.rstrip().split(' ', 1) for l in vocab_list]
 vocab.sort()
 
-while True:
+num_words = len(vocab)
+num_learned = 0
+
+while num_words - num_learned > 0:
     clear()
-    word, definition = random.choice(vocab)
+    selection  = random.choice(vocab)
+    selection_index = vocab.index(selection)
+    word, definition = selection
     definition = definition.strip('"')
 
-    print(word)
+    print(word, "\n\n%d/%d" % (num_learned, num_words))
     response = input()
     if response == "q":
         quit()
     clear()
 
     print(word, "-", definition)
+    print("\n%d/%d" % (num_learned, num_words))
     response = input()
     if response == "q":
         quit()
+    elif response == "y" or response == "'":
+        num_learned += 1
+        vocab = vocab[:selection_index] + vocab[selection_index + 1:]
 
-
+clear()
+print("You win!!")
