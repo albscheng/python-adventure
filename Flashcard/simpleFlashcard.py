306c7e8739e9ce1964719ea5502e33a495a91f44
albert c <albert.cheng@viasat.com>
2016-02-18 16:47:42 -0800

    added feature to track your worst words

--- Flashcard/simpleFlashcard.py
+++ Flashcard/simpleFlashcard.py
@@ -2,7 +2,9 @@
 import random
 import os
 
-clear = lambda: os.system('clear')
+NUM_WORST = 10
+
+clear = lambda: os.system('clear')              # Function to clear terminal
 
 with open('vocab.txt', 'r') as vocab_list:
     vocab = [l.rstrip().split(' ', 1) for l in vocab_list]
@@ -11,27 +13,39 @@ vocab.sort()
 num_words = len(vocab)
 num_learned = 0
 
+vocab_copy = vocab
+scores = [0] * num_words                # Creates list of 0s, size num_words
+
 while num_words - num_learned > 0:
     clear()
     selection  = random.choice(vocab)
     selection_index = vocab.index(selection)
     word, definition = selection
-    definition = definition.strip('"')
+    definition = definition.strip('"')  # Gets rid of " "
 
     print(word, "\n\n%d/%d" % (num_learned, num_words))
     response = input()
     if response == "q":
-        quit()
+        #quit()
+        break
     clear()
 
     print(word, "-", definition)
     print("\n%d/%d" % (num_learned, num_words))
     response = input()
     if response == "q":
-        quit()
+        #quit()
+        break
     elif response == "y" or response == "'":
         num_learned += 1
         vocab = vocab[:selection_index] + vocab[selection_index + 1:]
+    else:
+        scores[selection_index] += 1
 
 clear()
-print("You win!!")
+print("Your worst %d words are:" % (NUM_WORST))
+for i in range(0, NUM_WORST):
+    index_max = scores.index(max(scores))
+    word, definition = vocab_copy[index_max]  # Inefficient way to calc
+    scores[index_max] = -1                    # multiple maximums
+    print(word, "-", definition.strip('"'))
